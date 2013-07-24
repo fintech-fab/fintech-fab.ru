ea039c39c3c9adb386a983938e6d1bacf0f6edfc
e.barsova <e.barsova@fintech-fab.ru>
2013-07-24 16:00:12 +0400

    Новая страница - "заявка успешно отправлена"

---
+++ protected/views/site/pages/application-sent.php
@@ -0,0 +1,17 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+/*$this->breadcrumbs=array(
+	'Elecsnet',
+);*/
+?>
+<?php
+$this->widget('TopPageWidget');
+?>
+<div class="container container_12">
+	<div class="grid_12">
+		<h3>Ваша заявка отправлена!</h3>
+		<p>Ожидайте решения по займу. Если у вас есть вопросы - позвоните нам 8 (800) 555-75-78!</p>
+	</div>
+</div>
