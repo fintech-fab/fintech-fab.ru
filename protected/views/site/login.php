214c896f986ecd191560ae59067758375f48c542
popov <Fido707Net>
2013-07-29 15:35:17 +0400

    удаление лишних файлов, изменение верстки

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -8,45 +8,45 @@ $this->breadcrumbs=array(
 	'Вход',
 );
 ?>
-<div class="container container_12">
-	<div class="grid_12">
-
-<h1>Вход в систему</h1>
-
-<div class="form">
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'login-form',
-	'enableClientValidation'=>true,
-	'clientOptions'=>array(
-		'validateOnSubmit'=>true,
-	),
-)); ?>
-
-	<p class="note">Поля, отмеченные <span class="required">*</span> , являются обязательными.</p>
-
+<div class="container">
 	<div class="row">
-		<?php echo $form->labelEx($model,'username'); ?>
-		<?php echo $form->textField($model,'username'); ?>
-		<?php echo $form->error($model,'username'); ?>
-	</div>
+		<div class="span12">
+			<h1>Вход в систему</h1>
+			<div class="form">
+				<?php $form=$this->beginWidget('CActiveForm', array(
+					'id'=>'login-form',
+					'enableClientValidation'=>true,
+					'clientOptions'=>array(
+					'validateOnSubmit'=>true,
+					),
+				)); ?>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'password'); ?>
-		<?php echo $form->passwordField($model,'password'); ?>
-		<?php echo $form->error($model,'password'); ?>
-	</div>
+				<p class="note">Поля, отмеченные <span class="required">*</span> , являются обязательными.</p>
 
-	<div class="row rememberMe">
-		<?php echo $form->checkBox($model,'rememberMe'); ?>
-		<?php echo $form->label($model,'rememberMe'); ?>
-		<?php echo $form->error($model,'rememberMe'); ?>
-	</div>
+				<div class="row">
+					<?php echo $form->labelEx($model,'username'); ?>
+					<?php echo $form->textField($model,'username'); ?>
+					<?php echo $form->error($model,'username'); ?>
+				</div>
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Вход'); ?>
-	</div>
+				<div class="row">
+					<?php echo $form->labelEx($model,'password'); ?>
+					<?php echo $form->passwordField($model,'password'); ?>
+					<?php echo $form->error($model,'password'); ?>
+				</div>
 
-<?php $this->endWidget(); ?>
-</div><!-- form -->
-</div>
+				<div class="row rememberMe">
+					<?php echo $form->checkBox($model,'rememberMe'); ?>
+					<?php echo $form->label($model,'rememberMe'); ?>
+					<?php echo $form->error($model,'rememberMe'); ?>
+				</div>
+
+				<div class="row buttons">
+					<?php echo CHtml::submitButton('Вход'); ?>
+				</div>
+
+				<?php $this->endWidget(); ?>
+			</div><!-- form -->
+		</div>
+	</div>
 </div>
