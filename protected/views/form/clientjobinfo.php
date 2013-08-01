f1078faad0bac30e5773d25954b26632d357b438
e.barsova <e.barsova@fintech-fab.ru>
2013-08-01 10:22:06 +0400

    Блок "Выбранные условия" вынесен в виджет, а также исправлены представления

--- protected/views/form/clientjobinfo.php
+++ protected/views/form/clientjobinfo.php
@@ -33,24 +33,10 @@ $form = $this->beginWidget('application.components.utils.IkTbActiveForm', array(
 		<br/>
 		<? require dirname(__FILE__) . '/fields/job.php' ?>
 	</div>
-	<div class="row span5 conditions" >
-		<img src="<?php echo Yii::app()->request->baseUrl; ?>/static/img/00T.png"/>
-		<?php
-		$n = Dictionaries::$aDataTimes[Yii::app()->session['product']];
-		$d = new DateTime('now');
-		$d->add(new DateInterval('P'.$n.'D'));
-		$getDateToPayUntil = Dictionaries::$aDays[$d->format('w')].", ".$d->format('j')." ".Dictionaries::$aMonths[$d->format('n')]." ".$d->format('Y');
-		?>
-		<ul>
-			<li>Сумма займа: <span class="cost final_price"><?php echo Dictionaries::$aDataFinalPrices[Yii::app()->session['product']]?></span> рублей</li>
-			<li>Вернуть <span class="cost final_price"><?php echo Dictionaries::$aDataFinalPrices[Yii::app()->session['product']]?></span> рублей до: <span class="cost time">23:50</span>, <span class="cost date"><?php echo $getDateToPayUntil; ?></span></li>
-			<li>Стоимость подписки: <span class="cost price_count"><?php echo Dictionaries::$aDataPrices[Yii::app()->session['product']]?></span> рублей</li>
-			<li>Срок подписки: <span class="cost price_month"><?php echo Dictionaries::$aDataPriceCounts[Yii::app()->session['product']]?></span></li>
-			<li>Количество займов по подписке: <span class="cost count_subscribe"><?php echo Dictionaries::$aDataCounts[Yii::app()->session['product']]?></span></li>
-		</ul>
-	</div>
 
-	<div class="span2"><img src="<?php echo Yii::app()->request->baseUrl; ?>/static/img/step4.png"></div>
+	<?php $this->widget('ChosenConditionsWidget',array(
+		'curStep'=>Yii::app()->clientForm->getCurrentStep()+1,
+	)); ?>
 
 	<div class="clearfix"></div>
 
