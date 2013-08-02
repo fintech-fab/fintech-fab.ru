1c544bea13ea7d56b78d346fef760a34890adc9d
Владимир Юлдашев <misterio92@gmail.com>
2013-08-02 13:24:49 +0400

    fixes

--- protected/views/form/documents.php
+++ protected/views/form/documents.php
@@ -6,40 +6,74 @@ $this->pageTitle = Yii::app()->name;
 <?php
 $this->widget('TopPageWidget');
 ?>
-	<div class="row">
-		<div class="span12">
-			<div id="instructions" class="alert alert-info" style="font-weight: bold; font-size: 1.5em; padding: 10px;">
-				Разрешите использование
-				веб-камеры
-			</div>
+<div class="row">
+	<div class="span12">
+		<div id="instructions" class="alert alert-info" style="font-weight: bold; font-size: 1.5em; padding: 10px;">
+			Разрешите использование
+			веб-камеры
 		</div>
-
-		<div class="span12">
-			<div class="row">
-				<canvas id="inputCanvas" width="640" height="480" style="display: none"></canvas>
-				<video id="inputVideo" class="span6" width="100%" autoplay loop
-				       style="-webkit-transform: scaleX(-1); transform: scaleX(-1);"></video>
-
-				<img id="resultImage" class="span6" style="-webkit-transform: scaleX(-1); transform: scaleX(-1);"/>
-			</div>
-			<br/>
-
-			<div class="row">
-				<div class="span6">
-					<button id="shot-button" class="btn btn-primary" style="display: none;">сфотографировать</button>
-				</div>
-				<div class="span6">
-					<div id="confirm_text" class="alert alert-warning" style="display: none"></div>
-					<button id="confirm-shot-button" class="btn pull-right" style="display: none"
-					        onclick="confirmImage()">продолжить
-					</button>
-				</div>
-			</div>
-		</div>
-
 	</div>
 
+	<div class="span12">
+		<div class="row">
+			<canvas id="inputCanvas" width="640" height="480" style="display: none"></canvas>
+			<video id="inputVideo" class="span8" width="100%" autoplay loop
+			       style="-webkit-transform: scaleX(-1); transform: scaleX(-1);"></video>
 
+			<div class="span4">
+				<img id="exampleImage" width="100%" style="-webkit-transform: scaleX(-1); transform: scaleX(-1);"/><br/><br/>
+
+				<button data-toggle="modal" data-target="#confirm-modal" id="shot-button" class="btn btn-primary"
+				        style="display: none;">сфотографировать
+				</button>
+			</div>
+		</div>
+		<br/>
+
+		<div class="row">
+			<div class="span6">
+			</div>
+			<div class="span6">
+				<!--				<button id="confirm-shot-button" class="btn pull-right" style="display: none"-->
+				<!--				        onclick="confirmImage()">продолжить-->
+				<!--				</button>-->
+			</div>
+		</div>
+	</div>
+
+</div>
+
+<?php $this->beginWidget('bootstrap.widgets.TbModal', array('id' => 'confirm-modal')); ?>
+
+<div class="modal-header">
+	<a class="close" data-dismiss="modal">&times;</a>
+</div>
+
+<div class="modal-body">
+	<img id="resultImage" width="100%" style="-webkit-transform: scaleX(-1); transform: scaleX(-1);"/>
+</div>
+
+<div class="modal-footer">
+	<div id="confirm_text" class="alert alert-warning" style="text-align: left"></div>
+
+	<?php $this->widget('bootstrap.widgets.TbButton', array(
+		'label'       => 'Закрыть',
+		'url'         => '#',
+		'htmlOptions' => array('data-dismiss' => 'modal'),
+	)); ?>
+
+	<?php $this->widget('bootstrap.widgets.TbButton', array(
+		'type'        => 'primary',
+		'label'       => 'Продолжить',
+		'url'         => '#',
+		'htmlOptions' => array(
+			'data-dismiss' => 'modal',
+			'onClick'      => 'confirmImage()',
+		),
+	)); ?>
+</div>
+
+<?php $this->endWidget(); ?>
 
 <script type="text/javascript">
 	var currentDocument = {
@@ -60,6 +94,7 @@ $this->widget('TopPageWidget');
 	var ctx = canvas.getContext('2d');
 	var localMediaStream = null;
 
+	var exampleImage = $('#exampleImage');
 	var resultImage = $('#resultImage');
 
 	var onCameraFail = function (e) {
@@ -74,7 +109,7 @@ $this->widget('TopPageWidget');
 		localMediaStream = stream;
 
 		instructions.text(currentDocument.instructions);
-		resultImage.attr('src', currentDocument.example);
+		exampleImage.attr('src', currentDocument.example);
 
 		shotButton
 			.show()
@@ -109,40 +144,42 @@ $this->widget('TopPageWidget');
 		confirm_text.hide();
 
 		console.log(currentDocument.type);
-		$.post('/image/processPhoto', { image: resultImage.attr('src'), type: currentDocument.type, <?= Yii::app()->request->csrfTokenName ?>: '<?=Yii::app()->request->csrfToken?>' }, function (response) {
+		$.post('/image/processPhoto', { image: exampleImage.attr('src'), type: currentDocument.type, '<?= Yii::app()->request->csrfTokenName ?>': '<?=Yii::app()->request->csrfToken?>'}, function (response) {
 
-			var json = $.parseJSON(response);
+				var json = $.parseJSON(response);
 
-			shotButton
-				.removeAttr('disabled')
-				.text('Сфотографировать');
+				shotButton
+					.removeAttr('disabled')
+					.text('Сфотографировать');
 
-			//noinspection JSUnresolvedVariable
-			if (json.next_type === null) {
-				instructions.text('Все документы загружены');
+				//noinspection JSUnresolvedVariable
+				if (json.next_type === null) {
+					instructions.text('Все документы загружены');
 
-				setTimeout(function () {
-					window.location.href = '<?=Yii::app()->createUrl("pages/view",array('name'=>'formsent'));?>';
-				}, 2000);
+					setTimeout(function () {
+						window.location.href = '<?=Yii::app()->createUrl("pages/view",array('name'=>'formsent'));?>';
+					}, 2000);
 
-				shotButton.hide();
+					shotButton.hide();
 
-				return;
+					return;
+				}
+
+				//noinspection JSUnresolvedVariable
+				currentDocument = {
+					type: json.next_type.type,
+					title: json.next_type.title,
+					example: json.next_type.example,
+					instructions: json.next_type.instructions,
+					confirm_text: json.next_type.confirm_text
+				};
+
+				instructions.text(currentDocument.instructions);
+				exampleImage.attr('src', currentDocument.example);
+
 			}
-
-			//noinspection JSUnresolvedVariable
-			currentDocument = {
-				type: json.next_type.type,
-				title: json.next_type.title,
-				example: json.next_type.example,
-				instructions: json.next_type.instructions,
-				confirm_text: json.next_type.confirm_text
-			};
-
-			instructions.text(currentDocument.instructions);
-			resultImage.attr('src', currentDocument.example);
-
-		});
+		)
+		;
 	}
 
 </script>
