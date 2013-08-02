1df83a8f76a59208cb6333bb4f1d01c60c6b3e4b
popov <Fido707Net>
2013-08-02 17:56:46 +0400

    #806 правки по код ревью

--- protected/models/forms/ClientSelectGetWayForm.php
+++ protected/models/forms/ClientSelectGetWayForm.php
@@ -22,7 +22,7 @@ class ClientSelectGetWayForm extends ClientCreateFormAbstract
 			),
 			$aRequired
 		);
-		$aRules[]=array('get_way', 'in', 'range' => array_keys(Dictionaries::aWays(Yii::app()->session['product'])),'message' => 'Выберите правильный способ');
+		$aRules[]=array('get_way', 'in', 'range' => array_keys(Dictionaries::aWays(Yii::app()->session['ClientSelectProductForm']['product'])),'message' => 'Выберите правильный способ');
 
 		return $aRules;
 
