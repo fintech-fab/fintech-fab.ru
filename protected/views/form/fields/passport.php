86c6ece8fb97e4714e8df53139fb6fb402e419eb
e.barsova <e.barsova@fintech-fab.ru>
2013-08-05 13:58:43 +0400

    #808 серия и номер паспорта теперь маски, также прописаны maxlength и size

--- protected/views/form/fields/passport.php
+++ protected/views/form/fields/passport.php
@@ -4,8 +4,8 @@
  * @var IkTbActiveForm $form
  */
 
-echo $form->textFieldRow($oClientCreateForm, 'passport_series', array('class' => 'span1'));
-echo $form->textFieldRow($oClientCreateForm, 'passport_number', array('class' => 'span2'));
+echo $form->fieldMaskedRow($oClientCreateForm, 'passport_series', array('class' => 'span1', 'mask' => '9999', 'size'=>'4', 'maxlength'=>'4',));
+echo $form->fieldMaskedRow($oClientCreateForm, 'passport_number', array('class' => 'span2', 'mask' => '999999', 'size'=>'6', 'maxlength'=>'6',));
 
 echo $form->dateMaskedRow($oClientCreateForm, 'passport_date');
 
