2382c8e773de2db25f5a8c6b5acff0f7bef66752
popov <Fido707Net>
2013-08-09 19:05:48 +0400

    @test

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -2,17 +2,39 @@
  * глобальные настройки ajax-запросов
  */
 
-
+/*
 $(document).ready(function () {
 	$.ajaxSetup({
 		beforeSend: function () {
-			alertMessagesHandler.initLoading();
+			//alertMessagesHandler.initLoading();
 		},
 		complete: function () {
-			alertMessagesHandler.initComplete();
+			//alertMessagesHandler.initComplete();
 		},
 		error: function (jqXHR, textStatus, errorThrown) {
-			alertMessagesHandler.initError(errorThrown);
+			//alertMessagesHandler.initError(errorThrown);
 		}
 	});
-});
+});*/
+/*
+$(function() {
+	$.ajaxSetup({
+		error: function(jqXHR, exception) {
+			if (jqXHR.status === 0) {
+				alert('Not connect.\n Verify Network.');
+			} else if (jqXHR.status == 404) {
+				alert('Requested page not found. [404]');
+			} else if (jqXHR.status == 500) {
+				alert('Internal Server Error [500].');
+			} else if (exception === 'parsererror') {
+				alert('Requested JSON parse failed.');
+			} else if (exception === 'timeout') {
+				alert('Time out error.');
+			} else if (exception === 'abort') {
+				alert('Ajax request aborted.');
+			} else {
+				alert('Uncaught Error.\n' + jqXHR.responseText);
+			}
+		}
+	});
+});*/
