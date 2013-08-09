c12c9737adb65e3917eca473694522ce62118ecc
e.barsova <e.barsova@fintech-fab.ru>
2013-08-09 09:30:06 +0400

    #826

--- protected/components/StepsBreadCrumbsWidget.php
+++ protected/components/StepsBreadCrumbsWidget.php
@@ -3,26 +3,27 @@
 class StepsBreadCrumbsWidget extends CWidget
 {
 	public $crumbs = array(
-		array('Выбор суммы',array("/form/1")),
-		array('Выбор способа получения',array("/form/2")),
-		array('Личные данные',array("/form/3")),
-		array('Адрес',array("/form/4")),
-		array('Информация о работе',array("/form/5")),
-		array('Отправка',array("/form/6")),
+		array('Выбор суммы', array("/form/1"), 1),
+		array('Выбор способа получения', array("/form/2"), 2),
+		array('Идентификация', array("/form/identification"), 3),
+		array('Личные данные', array("/form/3"), 4),
+		array('Адрес', array("/form/4"), 4),
+		array('Информация о работе', array("/form/5"), 5),
+		array('Отправка', array("/form/6"), 6),
 	);
-	
-	public $divider = ' → ';
+
+	public $divider = '→';
 
 	public $htmlOptions = array(
 		'class' => 'breadcrumb',
-		'id' => 'steps',
+		'id'    => 'steps',
 	);
 
 	public $curStep; // номер текущего шага
 
 	public function run()
 	{
-		$this->curStep = Yii::app()->clientForm->getCurrentStep()+1;
+		$this->curStep = Yii::app()->clientForm->getCurrentStep() + 1;
 		$this->render('steps_bread_crumbs');
 	}
 }
--- protected/components/views/steps_bread_crumbs.php
+++ protected/components/views/steps_bread_crumbs.php
@@ -1,25 +1,21 @@
-<ul <?php echo ($this->htmlOptions['class'] ? ' class="' . $this->htmlOptions['class'] . '"' : ''); ?><?php echo ($this->htmlOptions['id'] ? ' id="' . $this->htmlOptions['id'] . '"' : ''); 	?> >
+<ul <?php echo($this->htmlOptions['class'] ? ' class="' . $this->htmlOptions['class'] . '"' : ''); ?><?php echo($this->htmlOptions['id'] ? ' id="' . $this->htmlOptions['id'] . '"' : ''); ?> >
 	<?php
 
 	$number = 1;
 
-	foreach($this->crumbs as $k => $crumb)
-	{
-		echo $number.'. ';
-		if($number <= $this->curStep) {
-			echo '<li class="done">'.CHtml::link($crumb[0],$crumb[1]).'</li>';
-		}
-		else
-		{
-			echo '<li><a href="">'.$crumb[0].'</a></li>';
+	foreach ($this->crumbs as $k => $crumb) {
+		echo $crumb[2] . '. ';
+		if ($number < $this->curStep) {
+			echo '<li class="done">' . CHtml::link($crumb[0], $crumb[1]) . '</li>';
+		} elseif ($number == $this->curStep) {
+			echo '<li class="active">' . CHtml::link($crumb[0], $crumb[1]) . '</li>';
+		} else {
+			echo '<li><a href="">' . $crumb[0] . '</a></li>';
 		}
 
-		if(sizeof($this->crumbs) > ($number))
-		{
+		if (sizeof($this->crumbs) > ($number)) {
 			echo '<span class="divider">' . $this->divider . '</span>';
 		}
-
-		//echo '</li>';
 
 		$number++;
 	}
