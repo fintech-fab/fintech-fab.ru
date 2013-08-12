5377e174ece420916b15f4608c85c42afa837d2e
e.barsova <e.barsova@fintech-fab.ru>
2013-08-12 10:14:20 +0400

    #821 в виджет добавлена ссылка на страницу загрузки совместимых с видеоидентификацией браузеров (Chrome и Firefox)

--- protected/models/Pages.php
+++ protected/models/Pages.php
@@ -4,19 +4,22 @@
  * This is the model class for table "tbl_pages".
  *
  * The followings are the available columns in table 'tbl_pages':
+ *
  * @property integer $page_id
- * @property string $page_name
- * @property string $page_title
- * @property string $page_content
+ * @property string  $page_name
+ * @property string  $page_title
+ * @property string  $page_content
  */
 class Pages extends CActiveRecord
 {
 	/**
 	 * Returns the static model of the specified AR class.
+	 *
 	 * @param string $className active record class name.
+	 *
 	 * @return Pages the static model class
 	 */
-	public static function model($className=__CLASS__)
+	public static function model($className = __CLASS__)
 	{
 		return parent::model($className);
 	}
@@ -38,14 +41,14 @@ class Pages extends CActiveRecord
 		// will receive user inputs.
 		return array(
 			array('page_name, page_title, page_content', 'required'),
-			array('page_name', 'length', 'max'=>20),
-			array('page_name', 'match','pattern'=>'/^[a-z][a-z0-9]+$/ui', 'message' => 'Имя может содержать только цифры и латинские символы, первым символом должна быть буква'),
-			array('page_title', 'length', 'max'=>100),
-			array('page_title', 'match','pattern'=>'/^[а-яёa-z0-9?,.!\-—: ]+$/ui', 'message' => 'Заголовок может содержать только буквы, цифры, знаки препинания и пробелы'),
-			array('page_name', 'unique', 'message'=>'Страница должна иметь уникальное имя'),
+			array('page_name', 'length', 'max' => 20),
+			array('page_name', 'match', 'pattern' => '/^[a-z][a-z0-9]+$/ui', 'message' => 'Имя может содержать только цифры и латинские символы, первым символом должна быть буква'),
+			array('page_title', 'length', 'max' => 100),
+			array('page_title', 'match', 'pattern' => '/^[а-яёa-z0-9?,.!\-—: ]+$/ui', 'message' => 'Заголовок может содержать только буквы, цифры, знаки препинания и пробелы'),
+			array('page_name', 'unique', 'message' => 'Страница должна иметь уникальное имя'),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
-			array('page_id, page_name, page_title, page_content', 'safe', 'on'=>'search'),
+			array('page_id, page_name, page_title, page_content', 'safe', 'on' => 'search'),
 		);
 	}
 
@@ -56,8 +59,7 @@ class Pages extends CActiveRecord
 	{
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
-		return array(
-		);
+		return array();
 	}
 
 	/**
@@ -66,15 +68,16 @@ class Pages extends CActiveRecord
 	public function attributeLabels()
 	{
 		return array(
-			'page_id' => 'ID страницы',
-			'page_name' => 'Имя страницы',
-			'page_title' => 'Заголовок страницы',
+			'page_id'      => 'ID страницы',
+			'page_name'    => 'Имя страницы',
+			'page_title'   => 'Заголовок страницы',
 			'page_content' => 'Содержимое страницы',
 		);
 	}
 
 	/**
 	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
 	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
 	 */
 	public function search()
@@ -82,24 +85,24 @@ class Pages extends CActiveRecord
 		// Warning: Please modify the following code to remove attributes that
 		// should not be searched.
 
-		$criteria=new CDbCriteria;
+		$criteria = new CDbCriteria;
 
-		$criteria->compare('page_id',$this->page_id);
-		$criteria->compare('page_name',$this->page_name,true);
-		$criteria->compare('page_title',$this->page_title,true);
-		$criteria->compare('page_content',$this->page_content,true);
+		$criteria->compare('page_id', $this->page_id);
+		$criteria->compare('page_name', $this->page_name, true);
+		$criteria->compare('page_title', $this->page_title, true);
+		$criteria->compare('page_content', $this->page_content, true);
 
 		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
+			'criteria' => $criteria,
 		));
 	}
 
 	protected function beforeValidate()
 	{
-		if(parent::beforeValidate())
-		{
+		if (parent::beforeValidate()) {
 			$this->page_title = trim($this->page_title);
 		}
+
 		return true;
 	}
 
@@ -107,11 +110,12 @@ class Pages extends CActiveRecord
 	{
 		$p = new CHtmlPurifier;
 		$p->options = array(
-			'Filter.YouTube'=>true,
-			'HTML.AllowedElements'=>array("div","p","ul","ol","li","h3","h4","h5","h6","img","a","b","i","s","span","u","em","strong","del","blockquote","sup","sub","pre","br","hr","table","tbody","thead","tr","td","th"),
-			'HTML.AllowedAttributes'=>array("img.src","img.alt","img.title","img.width","img.height","a.href","a.title","*.style","*.class"),
+			'Filter.YouTube'           => true,
+			'Attr.AllowedFrameTargets' => array('_blank', '_self', '_parent', '_top'),
+			'HTML.AllowedElements'     => array("div", "p", "ul", "ol", "li", "h3", "h4", "h5", "h6", "img", "a", "b", "i", "s", "span", "u", "em", "strong", "del", "blockquote", "sup", "sub", "pre", "br", "hr", "table", "tbody", "thead", "tr", "td", "th"),
+			'HTML.AllowedAttributes'   => array("img.src", "img.alt", "img.title", "img.width", "img.height", "a.href", "a.title", "a.target", "*.style", "*.class"),
 		);
-		$this->page_content=$p->purify($this->page_content);
+		$this->page_content = $p->purify($this->page_content);
 		parent::afterValidate();
 	}
 }
