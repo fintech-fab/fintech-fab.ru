e406bc800e1cf416847d41f5b6ff6c4fd94cf006
e.barsova <e.barsova@fintech-fab.ru>
2013-08-14 11:44:12 +0400

    @fix вид страницы выбора идентификации

--- protected/views/form/check_webcam.php
+++ protected/views/form/check_webcam.php
@@ -8,11 +8,10 @@
 
 	<?php Yii::app()->clientScript->registerCoreScript('jquery'); ?>
 </head>
-<body>
-<div style="width:400px">
-	<div id="instructions">Разрешите использование веб-камеры, если она есть, чтобы пройти видеоидентификацию</div>
+<body style="margin: 0px;">
+<div style="width:400px; text-align: center">
+	<video id="inputVideo" width="100%" autoplay loop style="-webkit-transform: scaleX(-1); transform: scaleX(-1);"></video>
 	<canvas id="inputCanvas" width="400" height="300" style="display: none; -webkit-transform: scaleX(-1); transform: scaleX(-1);"></canvas>
-	<video id="inputVideo" width="100%" autoplay loop style="height: 320px; -webkit-transform: scaleX(-1); transform: scaleX(-1);"></video>
 	<canvas id="overlay"></canvas>
 </div>
 <script type="text/javascript" src="<?= Yii::app()->request->baseUrl ?>/static/js/headtrackr.js"></script>
