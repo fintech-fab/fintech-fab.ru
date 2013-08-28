1094909761c708a514038f7ac9b4ac28ad07004b
popov <Fido707Net>
2013-08-28 17:52:22 +0400

    @fix

--- protected/components/utils/IkTbActiveForm.php
+++ protected/components/utils/IkTbActiveForm.php
@@ -2,48 +2,51 @@
 
 Yii::import('bootstrap.widgets.TbActiveForm');
 
-class IkTbActiveForm extends TbActiveForm {
+class IkTbActiveForm extends TbActiveForm
+{
 
 	/**
 	 * @param CModel|CActiveForm $oForm
-	 * @param $sAttribute
-	 * @param $aHtmlOptions
+	 * @param                    $sAttribute
+	 * @param                    $aHtmlOptions
+	 *
 	 * @return string
 	 */
-	public function phoneMaskedRow( CModel $oForm, $sAttribute, $aHtmlOptions = array() ){
+	public function phoneMaskedRow(CModel $oForm, $sAttribute, $aHtmlOptions = array())
+	{
 
-		if(empty($aHtmlOptions['mask'])){
+		if (empty($aHtmlOptions['mask'])) {
 			$aHtmlOptions['mask'] = '+7 999 999 99 99';
 		}
 
-		$sReturn  = '';
+		$sReturn = '';
 
-		if( empty( $aHtmlOptions['hideLabel'] ) ){
+		if (empty($aHtmlOptions['hideLabel'])) {
 			$sReturn .= $this->label(
 				$oForm,
 				$sAttribute,
 				array(
-					'required' => $oForm->isAttributeRequired( $sAttribute )
+					'required' => $oForm->isAttributeRequired($sAttribute)
 				)
 			);
 		}
 
 		$aDefaultOptions = array(
-			'size' => 10,
-			'maxlength' => 10
+			'size'      => 10,
+			'maxlength' => 16
 		);
 
-		if( !empty( $aHtmlOptions ) ){
+		if (!empty($aHtmlOptions)) {
 			$aDefaultOptions = array_merge($aDefaultOptions, $aHtmlOptions);
 		}
 
 		$sReturn .= $this->getController()->widget(
 			'CMaskedTextField',
 			array(
-				'model'			=> $oForm,
-				'attribute'		=> $sAttribute,
-				'mask'			=> $aHtmlOptions['mask'],
-				'htmlOptions'	=> $aDefaultOptions
+				'model'       => $oForm,
+				'attribute'   => $sAttribute,
+				'mask'        => $aHtmlOptions['mask'],
+				'htmlOptions' => $aDefaultOptions
 			),
 			true
 		);
@@ -55,14 +58,15 @@ class IkTbActiveForm extends TbActiveForm {
 	}
 
 
-	public function fieldPassportRow( CModel $oForm, $aAttributes, $aHtmlOptions = array() ){
+	public function fieldPassportRow(CModel $oForm, $aAttributes, $aHtmlOptions = array())
+	{
 
 		$aReturn = array();
 
 
 		foreach ($aAttributes as $sAttribute) {
-			if(empty($aHtmlOptions[$sAttribute])){
-				$aHtmlOptions[$sAttribute]=array();
+			if (empty($aHtmlOptions[$sAttribute])) {
+				$aHtmlOptions[$sAttribute] = array();
 			}
 
 			if (empty($aHtmlOptions[$sAttribute]['hideLabel'])) {
@@ -78,8 +82,8 @@ class IkTbActiveForm extends TbActiveForm {
 			$aDefaultOptions = array(
 				'size'      => '6',
 				'maxlength' => '6',
-				'mask' => '999999',
-				'class' => 'span2'
+				'mask'      => '999999',
+				'class'     => 'span2'
 			);
 
 			if (!empty($aHtmlOptions[$sAttribute])) {
@@ -99,10 +103,10 @@ class IkTbActiveForm extends TbActiveForm {
 				true
 			);
 
-			$aReturn[$sAttribute]['error']= $this->error($oForm, $sAttribute);
+			$aReturn[$sAttribute]['error'] = $this->error($oForm, $sAttribute);
 		}
 
-		$sReturn  = '<div style="display:table;">';
+		$sReturn = '<div style="display:table;">';
 		foreach ($aReturn as $sRet) {
 			$sReturn .= '<div style="display:table-cell;">';
 			$sReturn .= $sRet['label'];
@@ -132,40 +136,41 @@ class IkTbActiveForm extends TbActiveForm {
 	 * @return string
 	 */
 
-	public function fieldMaskedRow( CModel $oForm, $sAttribute, $aHtmlOptions = array() ){
+	public function fieldMaskedRow(CModel $oForm, $sAttribute, $aHtmlOptions = array())
+	{
 
-		if(empty($aHtmlOptions['mask'])){
+		if (empty($aHtmlOptions['mask'])) {
 			$aHtmlOptions['mask'] = '+7 999 999 99 99';
 		}
 
-		$sReturn  = '';
+		$sReturn = '';
 
-		if( empty( $aHtmlOptions['hideLabel'] ) ){
+		if (empty($aHtmlOptions['hideLabel'])) {
 			$sReturn .= $this->label(
 				$oForm,
 				$sAttribute,
 				array(
-					'required' => $oForm->isAttributeRequired( $sAttribute )
+					'required' => $oForm->isAttributeRequired($sAttribute)
 				)
 			);
 		}
 
 		$aDefaultOptions = array(
-			'size' => 16,
+			'size'      => 16,
 			'maxlength' => 16
 		);
 
-		if( !empty( $aHtmlOptions ) ){
-			$aDefaultOptions = array_merge($aDefaultOptions, $aHtmlOptions );
+		if (!empty($aHtmlOptions)) {
+			$aDefaultOptions = array_merge($aDefaultOptions, $aHtmlOptions);
 		}
 
 		$sReturn .= $this->getController()->widget(
 			'CMaskedTextField',
 			array(
-				'model'			=> $oForm,
-				'attribute'		=> $sAttribute,
-				'mask'			=> $aHtmlOptions['mask'],
-				'htmlOptions'	=> $aDefaultOptions
+				'model'       => $oForm,
+				'attribute'   => $sAttribute,
+				'mask'        => $aHtmlOptions['mask'],
+				'htmlOptions' => $aDefaultOptions
 			),
 			true
 		);
@@ -178,44 +183,46 @@ class IkTbActiveForm extends TbActiveForm {
 
 	/**
 	 * @param CModel|CActiveForm $oForm
-	 * @param $sAttribute
-	 * @param $aHtmlOptions
+	 * @param                    $sAttribute
+	 * @param                    $aHtmlOptions
+	 *
 	 * @return string
 	 */
-	public function dateMaskedRow( CModel $oForm, $sAttribute, $aHtmlOptions = array() ){
+	public function dateMaskedRow(CModel $oForm, $sAttribute, $aHtmlOptions = array())
+	{
 
-		if(empty($aHtmlOptions['mask'])){
+		if (empty($aHtmlOptions['mask'])) {
 			$aHtmlOptions['mask'] = '99.99.9999';
 		}
 
-		$sReturn  = '';
+		$sReturn = '';
 
-		if( empty( $aHtmlOptions['hideLabel'] ) ){
+		if (empty($aHtmlOptions['hideLabel'])) {
 			$sReturn .= $this->label(
 				$oForm,
 				$sAttribute,
 				array(
-					'required' => $oForm->isAttributeRequired( $sAttribute )
+					'required' => $oForm->isAttributeRequired($sAttribute)
 				)
 			);
 		}
 
 		$aDefaultOptions = array(
-			'size' => 10,
+			'size'      => 10,
 			'maxlength' => 10
 		);
 
-		if( !empty( $aHtmlOptions ) ){
-			$aDefaultOptions = array_merge($aDefaultOptions, $aHtmlOptions );
+		if (!empty($aHtmlOptions)) {
+			$aDefaultOptions = array_merge($aDefaultOptions, $aHtmlOptions);
 		}
 
 		$sReturn .= $this->getController()->widget(
 			'CMaskedTextField',
 			array(
-				'model'			=> $oForm,
-				'attribute'		=> $sAttribute,
-				'mask'			=> $aHtmlOptions['mask'],
-				'htmlOptions'	=> $aDefaultOptions
+				'model'       => $oForm,
+				'attribute'   => $sAttribute,
+				'mask'        => $aHtmlOptions['mask'],
+				'htmlOptions' => $aDefaultOptions
 			),
 			true
 		);
@@ -226,71 +233,70 @@ class IkTbActiveForm extends TbActiveForm {
 
 	}
 
-	public function dateFieldRow( CModel $oForm, $sAttribute, $aHtmlOptions = array() )
+	public function dateFieldRow(CModel $oForm, $sAttribute, $aHtmlOptions = array())
 	{
 		$sReturn = '';
 
-		if( empty( $aHtmlOptions['hideLabel'] ) ){
+		if (empty($aHtmlOptions['hideLabel'])) {
 			$sReturn .= $this->label(
 				$oForm,
 				$sAttribute,
 				array(
-					'required' => $oForm->isAttributeRequired( $sAttribute )
+					'required' => $oForm->isAttributeRequired($sAttribute)
 				)
 			);
 		}
 
-		$aDefaultOptions = array(
-		);
+		$aDefaultOptions = array();
 
-		if( !empty( $aHtmlOptions ) ){
+		if (!empty($aHtmlOptions)) {
 			$aDefaultOptions = array_merge($aDefaultOptions, $aHtmlOptions);
 		}
 
 		return
 			$sReturn .
-			$this->dateField($oForm, $sAttribute, $aDefaultOptions ) .
-			$this->error($oForm, $sAttribute )
-		;
+			$this->dateField($oForm, $sAttribute, $aDefaultOptions) .
+			$this->error($oForm, $sAttribute);
 
 	}
 
 
-	public function fieldCombineDaysHours( CModel $oForm, $sAttribute, $sLabel, $aHtmlOptions = array() ){
+	public function fieldCombineDaysHours(CModel $oForm, $sAttribute, $sLabel, $aHtmlOptions = array())
+	{
 
 		$sReturn = '';
-		if( empty( $aHtmlOptions['hideLabel'] ) ){
+		if (empty($aHtmlOptions['hideLabel'])) {
 			$sReturn .= $this->label(
 				$oForm,
 				$sAttribute,
 				array(
-					'class' => 'span2',
-					'label' => $sLabel,
-					'required' => $oForm->isAttributeRequired( $sAttribute .'_days' ),
+					'class'    => 'span2',
+					'label'    => $sLabel,
+					'required' => $oForm->isAttributeRequired($sAttribute . '_days'),
 				)
 			);
 		}
 
 		$aDays = array();
 		$i = 0;
-		while( $i <= 180 ){
+		while ($i <= 180) {
 			$aDays[$i] = $i . ' д.';
 			$i++;
 		}
 
 		$aHours = array();
 		$i = 0;
-		while( $i <= 23 ){
+		while ($i <= 23) {
 			$aHours[$i] = $i . ' ч.';
 			$i++;
 		}
 
 		$sReturn .= '<span class="span2">';
-			$sReturn .= $this->dropDownList( $oForm, $sAttribute . '_days', $aDays, array( 'class' => 'span1' ) );
-			$sReturn .= '&nbsp;';
-			$sReturn .= $this->dropDownList( $oForm, $sAttribute . '_hours', $aHours, array( 'class' => 'span1' ) );
+		$sReturn .= $this->dropDownList($oForm, $sAttribute . '_days', $aDays, array('class' => 'span1'));
+		$sReturn .= '&nbsp;';
+		$sReturn .= $this->dropDownList($oForm, $sAttribute . '_hours', $aHours, array('class' => 'span1'));
 		$sReturn .= '</span>';
-		$sReturn .= $this->error( $oForm, $sAttribute . '_days' );
+		$sReturn .= $this->error($oForm, $sAttribute . '_days');
 
 		return $sReturn;
 
