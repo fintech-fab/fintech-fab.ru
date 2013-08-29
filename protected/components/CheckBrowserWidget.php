25ced12bd2b7b6039a4f1eb91b79aa49111d1acd
e.barsova <e.barsova@fintech-fab.ru>
2013-08-29 12:10:20 +0400

    @fix переделать виджет, а то одно сообщение тут, второе - в browser.js

--- protected/components/CheckBrowserWidget.php
+++ protected/components/CheckBrowserWidget.php
@@ -5,10 +5,11 @@ class CheckBrowserWidget extends CWidget
 	/**
 	 * @var string выводимое сообщение
 	 */
-	//TODO переделать виджет, а то одно сообщение тут, второе - в browser.js
-	public $sMessage = "<strong>Внимание!</strong> Во время заполнения анкеты Вы можете пройти
-видеоидентификацию, которая работает только в браузерах <strong>Chrome</strong> и <strong>Firefox</strong>
-последних версий.";
+	public $sMessage = "<strong>Внимание!</strong> Во время заполнения анкеты Вы можете пройти видеоидентификацию, которая работает только в браузерах <strong>Chrome</strong> и <strong>Firefox</strong> последних версий.";
+	/**
+	 * @var string выводимое сообщение для мобильного телефона
+	 */
+	public $sMobileMessage = " <strong>Внимание!</strong> Во время заполнения анкеты Вы можете пройти видеоидентификацию. Видеоидентификация работает только <strong>на компьютере</strong>, в браузерах Chrome и Firefox последних версий.";
 
 	public $aHtmlOptions = array();
 
