b1a99a1fbbf9cafd0cdb29b32466170f00d5d1bf
popov <Fido707Net>
2013-09-18 15:15:14 +0400

    #1061

--- protected/config/examples/log.php
+++ protected/config/examples/log.php
@@ -5,6 +5,19 @@ return array(
 		array(
 			'class'  => 'CFileLogRoute',
 			'levels' => 'error, warning',
-		)
+		),
+		array(
+			'class'         => 'CWebLogRoute',
+			'categories'    => 'application',
+			'levels'        => 'error, warning, trace, profile, info',
+			'showInFireBug' => true
+		),
+		array(
+			'class'        => 'CDbLogRoute',
+			'levels'       => 'error, warning, trace',
+			'connectionID' => 'db',
+			'logTableName' => 'tbl_log',
+
+		),
 	)
 );
