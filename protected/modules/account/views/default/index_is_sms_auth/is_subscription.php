edd6e2f3e9877033058c89f0988bc02020230949
e.barsova <e.barsova@fintech-fab.ru>
2013-09-24 14:04:53 +0400

    @fix формулировки

--- protected/modules/account/views/default/index_is_sms_auth/is_subscription.php
+++ protected/modules/account/views/default/index_is_sms_auth/is_subscription.php
@@ -21,14 +21,26 @@ $this->pageTitle = Yii::app()->name . ' - Личный кабинет - Сост
 	?>
 	<strong>Баланс:</strong>  <?= Yii::app()->adminKreddyApi->getBalance(); ?> руб. <br />
 <?php } ?>
+
 <strong>Пакет:</strong> <?= Yii::app()->adminKreddyApi->getSubscriptionProduct() ?><br />
-<strong>Статус:</strong> <?= Yii::app()->adminKreddyApi->getStatusMessage() ?><br />    <strong>Пакет активен
-	до:</strong>  <?=
+
+<strong>Статус:</strong> <?= Yii::app()->adminKreddyApi->getStatusMessage() ?><br />
+
+<?php if (Yii::app()->adminKreddyApi->getActiveLoanExpiredTo()) {
+	// если есть займ, выводим дату возврата
+	?>
+	<strong>Возврат займа:</strong> <?= Yii::app()->adminKreddyApi->getActiveLoanExpiredTo() ?><br />
+<?php } ?>
+
+<strong>Пакет активен до:</strong>  <?=
 (Yii::app()->adminKreddyApi->getSubscriptionActivity()) ?
 	Yii::app()->adminKreddyApi->getSubscriptionActivity()
 	: "&mdash;"; ?>
-<br /><strong>Доступно займов:</strong> <?= Yii::app()->adminKreddyApi->getSubscriptionAvailableLoans(); ?><br />
-<?php //TODO: проверять, что есть доступные займы + время убрать
+<br />
+
+<strong>Доступно займов:</strong> <?= Yii::app()->adminKreddyApi->getSubscriptionAvailableLoans(); ?><br />
+
+<?php
 // если есть мораторий на займ и ещё есть доступные займы, выводим соответствующее сообщение
 if (Yii::app()->adminKreddyApi->getMoratoriumLoan() && Yii::app()->adminKreddyApi->getSubscriptionAvailableLoans() > 0) {
 	?>
