32968f8df04c528a3ed2b976d58dec7a0450a657
e.barsova <e.barsova@fintech-fab.ru>
2013-09-30 11:59:40 +0400

    @fix виджет проверки браузера - в ЛК. Попутно немножко подправила представление виджета

--- protected/components/views/check_browser_widget.php
+++ protected/components/views/check_browser_widget.php
@@ -6,15 +6,16 @@
 
 ?>
 
-<div class="alert in alert-block alert-error hide" id="browserFormat">
+	<div class="alert in alert-block alert-error hide" id="browserFormat">
 	<span id="attention_message">
 		<?= $this->sMessage ?>
 	</span> <br /><br /><span id="your_browser"></span>
 
-	<div id="get_browser" style="margin-top:10px;"><a href="<?= Yii::app()
-			->createUrl('/pages/view/browser') ?>" target="_blank">Перейти на страницу загрузки поддерживаемого
-			браузера &raquo;</a></div>
-
+		<div id="get_browser" style="margin-top:10px;"><a href="<?=
+			Yii::app()
+				->createUrl('/pages/view/browser') ?>" target="_blank">Перейти на страницу загрузки поддерживаемого
+				браузера &raquo;</a></div>
+	</div>
 <?php
 
 Yii::app()->clientScript->registerScript('messageForBrowser', '
