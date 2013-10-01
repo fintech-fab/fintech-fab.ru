a1eade7aea306623af6032485db0c14043bf77f2
e.barsova <e.barsova@fintech-fab.ru>
2013-10-01 15:24:15 +0400

    #1220

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -29,6 +29,19 @@
 <div id="ClickTaleDiv" style="display: none;"></div>
 <!--  header navbar start -->
 <?php $this->beginContent('//layouts/main_navbar_top');
+
+$this->widget(
+	'bootstrap.widgets.TbSelect2',
+	array(
+		'name'    => 'emptydata',
+		'data'    => null,
+		'options' => array(
+			'placeholder' => 'type clever, or is, or just type!',
+			'width'       => '40%',
+		)
+	)
+);
+
 echo $content;
 $this->endContent();
 ?>
