39ecb0a6fc9107e59c54cf9e09948acd1a5d8c42
popov <Fido707Net>
2013-10-03 10:52:47 +0400

    #1200 geoip

--- protected/components/UserCityWidget.php
+++ protected/components/UserCityWidget.php
@@ -7,6 +7,11 @@ class UserCityWidget extends CWidget
 	{
 		$oCityIdCookie = Yii::app()->request->cookies['city_id'];
 
+		/**
+		 * TODO прочитать комментарий ниже
+		 * в куки писать ВСЮ инфу, в т.ч. название города и региона, и когда в куке инфа есть - не читать из БД!
+		 */
+
 		if (!empty($oCityIdCookie)) { // если в куках есть id города, выводим его
 			$this->sCityName = CitiesRegions::getCityNameById($oCityIdCookie->value);
 		} elseif (ids_ipGeoBase::getCityByIP()) { // если удалось определить город по ip
