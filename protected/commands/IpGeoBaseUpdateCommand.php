d6dd4e32afcb8d63519501c263700bd20da035d3
popov <Fido707Net>
2013-10-11 09:40:19 +0400

    @fix

--- protected/commands/IpGeoBaseUpdateCommand.php
+++ protected/commands/IpGeoBaseUpdateCommand.php
@@ -11,7 +11,7 @@ class IpGeoBaseUpdateCommand extends CConsoleCommand
 		$ch = curl_init();
 		$fp = fopen('geo_files.tar.gz', 'w');
 
-		curl_setopt($ch, CURLOPT_URL,'http://ipgeobase.ru/files/db/Main/geo_files.tar.gz');
+		curl_setopt($ch, CURLOPT_URL, 'http://ipgeobase.ru/files/db/Main/geo_files.tar.gz');
 		curl_setopt($ch, CURLOPT_TIMEOUT, 50);
 		curl_setopt($ch, CURLOPT_FILE, $fp);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
@@ -30,53 +30,41 @@ class IpGeoBaseUpdateCommand extends CConsoleCommand
 		ignore_user_abort(1); // указываем, чтобы скрипт продолжал работать даже при разрыве
 
 		// проверяем наличие файла cities.txt в папке рядом с этим скриптом
-		if(file_exists('cities.txt'))
-		{
+		if (file_exists('cities.txt')) {
 			$sSqlQuery = "TRUNCATE TABLE `tbl_geo__cities`;";
 			Yii::app()->db->createCommand($sSqlQuery)->execute();
 
 			$file = file('cities.txt');
 			$pattern = '#(\d+)\s+(.*?)\t+(.*?)\t+(.*?)\t+(.*?)\s+(.*)#';
-			foreach ($file as $row)
-			{
+			foreach ($file as $row) {
 				$row = iconv('windows-1251', 'utf-8', $row);
-				if(preg_match($pattern, $row, $out))
-				{
+				if (preg_match($pattern, $row, $out)) {
 					$sSqlQuery = "INSERT INTO `tbl_geo__cities` (`city_id`, `city`, `region`, `district`, `lat`, `lng`) VALUES('$out[1]', '$out[2]', '$out[3]', '$out[4]', '$out[5]', '$out[6]');";
 					Yii::app()->db->createCommand($sSqlQuery)->execute();
 				}
 
 			}
-		}else
-		{
+			unlink('cities.txt');
+		} else {
 			echo 'Ошибка! Файл cities.txt должен лежать рядом с этим файлом!';
 		}
 
 // проверяем наличие файла cidr_optim.txt в папке рядом с этим скриптом
-		if(file_exists('cidr_optim.txt'))
-		{
+		if (file_exists('cidr_optim.txt')) {
 			$sSqlQuery = "TRUNCATE TABLE `tbl_geo__base`;";
 			Yii::app()->db->createCommand($sSqlQuery)->execute();
 
 			$file = file('cidr_optim.txt');
 			$pattern = '#(\d+)\s+(\d+)\s+(\d+\.\d+\.\d+\.\d+)\s+-\s+(\d+\.\d+\.\d+\.\d+)\s+(\w+)\s+(\d+|-)#';
-			foreach ($file as $row)
-			{
-				if(preg_match($pattern, $row, $out))
-				{
+			foreach ($file as $row) {
+				if (preg_match($pattern, $row, $out)) {
 					$sSqlQuery = "INSERT INTO `tbl_geo__base` (`long_ip1`, `long_ip2`, `ip1`, `ip2`, `country`, `city_id`) VALUES('$out[1]', '$out[2]', '$out[3]', '$out[4]', '$out[5]', '$out[6]');";
 					Yii::app()->db->createCommand($sSqlQuery)->execute();
 				}
 			}
-
-		}else
-		{
+			unlink('cidr_optim.txt');
+		} else {
 			echo 'Ошибка! Файл cidr_optim.txt должен лежать рядом с этим файлом!';
 		}
-
-
-		unlink('cidr_optim.txt');
-		unlink('cities.txt');
-
 	}
 }
