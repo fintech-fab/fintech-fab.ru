afc995505f451c0344ec9c127ebfc4c674232010
popov <Fido707Net>
2013-10-11 16:48:09 +0400

    @add slider widget

---
+++ protected/components/SliderWidget.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Class SliderWidget
+ */
+
+class SliderWidget extends CWidget
+{
+
+	public function run()
+	{
+		$this->render('slider_widget');
+	}
+
+}
---
+++ protected/components/views/slider_widget.php
@@ -0,0 +1,58 @@
+<?php
+/* @var CWidget $this */
+/* @var LoginForm $model */
+/* @var IkTbActiveForm $form */
+?>
+
+<div class="row">
+	<?php
+	$form = $this->beginWidget('application.components.utils.IkTbActiveForm',
+		array(
+			'id'                   => 'slider-form',
+			'enableAjaxValidation' => true,
+			'type'                 => 'horizontal',
+			'clientOptions'        => array(
+				'validateOnChange' => false,
+				'validateOnSubmit' => true,
+			),
+			'action'               => Yii::app()->createUrl('/form/'),
+		));
+	//TODO сделать нормальную модель
+	$model = new LoginForm();
+
+
+	$this->widget('zii.widgets.jui.CJuiSlider', array(
+		'id'             => 'slider',
+		'value'          => 50, 'options' => array(
+			'min'   => 1000,
+			'max'   => 10000,
+			'step'  => 500,
+			'slide' => 'js:function(event, ui) {
+			    $("#TextBoxId").val(ui.value);
+			    $("#slider .ui-slider-handle").find(".tooltip-inner").html(ui.value);
+			    $(".cost.final_price").html(ui.value);
+
+			    var n = 7;
+			    if(ui.value>=5000) n = 14;
+
+				$(".cost.date").html(getDateToPayUntil(n));
+
+			 }'
+		), 'htmlOptions' => array(
+			'style' => 'height:12px;',
+		),
+	));
+
+	?>
+	<script type="text/javascript">
+		jQuery(window).on('load',function() {
+			$("#slider .ui-slider-handle").tooltip({title: 'test', placement: 'bottom', trigger: 'manual', container: '#slider .ui-slider-handle'}).tooltip('show');
+		});
+	</script>
+
+
+
+	<?= $form->hiddenField($model, 'username', array('id' => 'TextBoxId')); ?>
+
+	<?php $this->endWidget(); ?>
+</div>
