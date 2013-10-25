22b9174ec5728048081b850fd5492fbf66169e16
popov <Fido707Net>
2013-10-25 17:15:44 +0400

    @fix

--- protected/components/views/top_page.php
+++ protected/components/views/top_page.php
@@ -1,15 +1,6 @@
 <div class="container top_page">
 	<div class="row">
 		<div class="hero-unit">
-			<!--p class="intro"><strong>Подключись к сервису «Кредди» сейчас и расширь свои финансовые возможности!</strong></p>
-
-			<ul>
-				<li>Пакеты займов до <strong>10000 рублей</strong></li>
-				<li>Получение займа <strong>не выходя из дома</strong></li>
-				<li><strong>На мобильный телефон</strong> или карту «Кредди»</li>
-				<li>Оплата только <strong>за подключение</strong></li>
-				<li>Просто взять - <strong>просто вернуть!</strong></li>
-			</ul-->
 			<?php $this->widget(
 				'application.components.utils.IkTbCarousel',
 				array(
