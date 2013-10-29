a038429119e2af77db8dd848ebb0b30e639afa47
popov <Fido707Net>
2013-10-29 17:17:50 +0400

    #1336

--- protected/components/views/top_page.php
+++ protected/components/views/top_page.php
@@ -11,21 +11,21 @@
 					),
 					'items'   => array(
 						array(
-							'image' => CHtml::encode('/static/img/carousel/kreddy-web-01.jpg'),
+							'image' => CHtml::encode('/static/img/kreddy-carousel/kreddy-web-01.jpg'),
 							'label' => 'Быстро! Удобно! Дистанционно!',
 						),
 						array(
-							'image' => CHtml::encode('/static/img/carousel/kreddy-web-02.jpg'),
+							'image' => CHtml::encode('/static/img/kreddy-carousel/kreddy-web-02.jpg'),
 							'label' => 'Мобильный нужен не только для разговоров',
 							'url'=> '/pages/view/check'
 						),
 						array(
-							'image' => CHtml::encode('/static/img/carousel/kreddy-web-03.jpg'),
+							'image' => CHtml::encode('/static/img/kreddy-carousel/kreddy-web-03.jpg'),
 							'label' => 'Не отказывай себе в маленьких радостях!',
 							'url'=> '/form/shopping'
 						),
 						array(
-							'image' => CHtml::encode('/static/img/carousel/kreddy-web-04.jpg'),
+							'image' => CHtml::encode('/static/img/kreddy-carousel/kreddy-web-04.jpg'),
 							'label' => 'Выбери свой пакет займов!',
 						),
 					),
