70a498f88b21ee93f388758824f6673cfaa072d5
e.barsova <e.barsova@fintech-fab.ru>
2013-11-27 14:30:29 +0400

    #1408

--- cron/sh/mysql-clear-base.sh
+++ cron/sh/mysql-clear-base.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-php ./../cron.php anketasender sendAnketa
+php ./../cron.php clientdatasender sendClientData
 php ./../cron.php dbcleaner clearDb
 php ./../cron.php ipgeobaseupdate downloadDb
 php ./../cron.php ipgeobaseupdate updateDb
--- protected/commands/ClientDataSenderCommand.php
+++ protected/commands/ClientDataSenderCommand.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Class AnketaSenderCommand
+ * Class ClientDataSenderCommand
  */
-class AnketaSenderCommand extends CConsoleCommand
+class ClientDataSenderCommand extends CConsoleCommand
 {
 	/**
 	 * Тело отправляемого письма: ФИО, E-mail и телефон клиентов, не до конца заполнивших анкету
@@ -11,26 +11,29 @@ class AnketaSenderCommand extends CConsoleCommand
 	 */
 	public $sClientsInfo = '';
 
-	public function actionSendAnketa()
+	public function actionSendClientData()
 	{
-		// выбираем только тех, кого ещё не "чистили"
+		// выбираем только за последний день тех, кого ещё не "чистили"
 		$sSql = "SELECT * from `tbl_client` WHERE `flag_sms_confirmed` = '0'
-		AND `dt_update` <= TIMESTAMP((NOW() - INTERVAL 1 DAY))
+		AND `dt_update` >= TIMESTAMP((NOW() - INTERVAL 1 DAY))
 		AND `last_name` != '*'
 		AND `first_name` != '*'
 		AND `third_name` != '*'
 		";
 
 		$oDataReader = Yii::app()->db->createCommand($sSql)->query();
+		$oClient = $oDataReader->read();
 
-		while (($oClient = $oDataReader->read()) !== false) {
-			$sLastName = (!empty($oClient['last_name'])) ? $oClient['last_name'] : '-';
-			$sFirstName = (!empty($oClient['first_name'])) ? $oClient['first_name'] : '-';
-			$sThirdName = (!empty($oClient['third_name'])) ? $oClient['third_name'] : '-';
+		while ($oClient !== false) {
+			$sLastName = (!empty($oClient['last_name'])) ? $oClient['last_name'] : '';
+			$sFirstName = (!empty($oClient['first_name'])) ? $oClient['first_name'] : '';
+			$sThirdName = (!empty($oClient['third_name'])) ? $oClient['third_name'] : '';
 			$sEmail = (!empty($oClient['email'])) ? $oClient['email'] : '-';
 			$sPhone = (!empty($oClient['phone'])) ? $oClient['phone'] : '-';
 
 			$this->sClientsInfo .= "Телефон: $sPhone\r\nE-mail: $sEmail\r\nФИО: $sLastName $sFirstName $sThirdName \r\n\r\n=================================================\r\n\r\n";
+
+			$oClient = $oDataReader->read();
 		}
 
 		$sEmail = SiteParams::getContactEmail();
