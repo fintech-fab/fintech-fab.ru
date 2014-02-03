8bd9c1fdf0d28324a419c50222c35a2cbe5abd93
popov <Fido707Net>
2014-02-03 16:56:53 +0400

    #1863 @fix one issue

--- protected/modules/account/views/default/app_info.php
+++ protected/modules/account/views/default/app_info.php
@@ -1,5 +1,4 @@
 <div class="alert in alert-block alert-info span7">
 	<h4>Не работает веб-камера?</h4><br /> Пройдите идентификацию на своем смартфоне!
-	<br /><a href="https://play.google.com/store/apps/details?id=ru.kreddy" target="_blank">
-		<img alt="Get it on Google Play" src="/static/images/ru_generic_rgb_wo_45.png" /> </a>
+	<br /><br /><a href="https://play.google.com/store/apps/details?id=ru.kreddy" target="_blank"> <img alt="Get it on Google Play" src="/static/images/ru_generic_rgb_wo_45.png" /> </a>
 </div>
