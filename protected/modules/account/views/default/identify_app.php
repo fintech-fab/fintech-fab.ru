7125f90d176640560f2ef6594dc8e2e897e49531
popov <Fido707Net>
2014-02-03 15:42:20 +0400

    #1863 @fix some issues

--- protected/modules/account/views/default/identify_app.php
+++ protected/modules/account/views/default/identify_app.php
@@ -9,9 +9,9 @@ $this->pageTitle = Yii::app()->name . " - Идентификация на сма
 	<h4>Не работает веб-камера?</h4><br /> Пройдите идентификацию на своем смартфоне!<br /> Скачайте приложение прямо
 	сейчас!<br />
 
-	<div class=""><br /><a href="https://play.google.com/store/apps/details?id=ru.kreddy" target="_blank">
-			<img alt="Get it on Google Play" src="/static/images/ru_generic_rgb_wo_45.png" /> </a></div>
-	Приложение доступно для смартфонов и планшетов с платформой Android (Samsung, HTC, Sony, Alcatel и другие).
+	<br /><a href="https://play.google.com/store/apps/details?id=ru.kreddy" target="_blank">
+		<img alt="Get it on Google Play" src="/static/images/ru_generic_rgb_wo_45.png" /> </a> Приложение доступно для
+	смартфонов и планшетов с платформой Android (Samsung, HTC, Sony, Alcatel и другие).
 	<?php if (!Yii::app()->adminKreddyApi->isFirstIdentification()): ?>
 		<br /><br />После идентификации потребуется ввести данные документов, использованных при идентификации.
 	<?php endif; ?>
