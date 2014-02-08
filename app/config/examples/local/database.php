50a08dafcf6ef70129fb84c323517e256b24fff1
mikanoz <mikhail.e.novikov@gmail.com>
2014-02-09 00:14:28 +0400

    hello world

---
+++ app/config/examples/local/database.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+
+	'connections' => array(
+
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => '%database%',
+			'username'  => '%username%',
+			'password'  => '%password%',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_general_ci',
+			'prefix'    => '',
+		),
+
+	),
+
+);
