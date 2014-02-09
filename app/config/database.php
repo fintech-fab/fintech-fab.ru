2886a388215bb049b713575808f0eac176e7aea4
mikanoz <mikhail.e.novikov@gmail.com>
2014-02-09 10:19:49 +0400

    настройки коннекта mysql по умолчанию

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'dev',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'fintechfab',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
