21431730d48a2d45769199e36e43c42e4791d5bf
d.laptev <d.laptev@fintech-lab.ru>
2014-02-12 12:13:15 +0400

    #1701 Готов полный функционал задачи

---
+++ protected/modules/account/views/default/change_sms_auth_setting/sms_auth_setting_form.php
@@ -0,0 +1,35 @@
+<?php
+/* @var DefaultController $this */
+/* @var ChangeSmsAuthSettingForm $oChangeSmsAuthSettingForm */
+/* @var IkTbActiveForm $form */
+
+$this->pageTitle = Yii::app()->name . " - Включение/Выключение дополнительной аутентификации по смс";
+?>
+	<h4>Включение/Выключение дополнительной аутентификации по смс</h4>
+
+<?php $form = $this->beginWidget('application.components.utils.IkTbActiveForm', array(
+	'id'                   => 'sms-auth-setting-form',
+	'enableAjaxValidation' => false,
+	'type'                 => 'horizontal',
+	'action'               => Yii::app()->createUrl('/account/changeSmsAuthSetting'),
+));
+?>
+
+	<div class="row">
+		<div class="span5">
+			<?= $form->checkBoxRow($oChangeSmsAuthSettingForm, 'sms_auth_enabled'); ?>
+		</div>
+	</div>
+
+	<div class="clearfix"></div>
+	<div class="form-actions">
+		<?php $this->widget('bootstrap.widgets.TbButton', array(
+			'buttonType' => 'submit',
+			'type'       => 'primary',
+			'size'       => 'normal',
+			'label'      => 'Отправить',
+		)); ?>
+	</div>
+
+<?php
+$this->endWidget();
