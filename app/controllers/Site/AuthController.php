2825527e7289be6579ade86831433271485658e4
finking <finking@list.ru>
2014-03-06 23:23:55 +0400

    Начало доработки по VK.

--- app/controllers/Site/AuthController.php
+++ app/controllers/Site/AuthController.php
@@ -1,8 +1,12 @@
 <?php
-
+/**
+ * @var array $userInfo
+ */
 namespace App\Controllers\Site;
 
 use App\Controllers\BaseController;
+use Config;
+use FintechFab\Models\UserVk;
 use Input;
 use Redirect;
 
@@ -34,5 +38,59 @@ class AuthController extends BaseController
 
 		return $data;
 
+	}
+
+	public function vk()
+	{
+		$client_id = Config::get('vk.ID_vk'); // ID приложения
+		$client_secret = Config::get('vk.key_vk'); // Защищённый ключ
+		$redirect_uri = Config::get('vk.url'); // Адрес сайта
+		$code = Input::get('code');
+
+		if (isset($code)) {
+			$result = false;
+			$params = array(
+				'client_id'     => $client_id,
+				'client_secret' => $client_secret,
+				'code'          => $code,
+				'redirect_uri'  => $redirect_uri
+			);
+
+			$token = json_decode(file_get_contents('https://oauth.vk.com/access_token' . '?' . urldecode(http_build_query($params))), true);
+
+			if (isset($token['access_token'])) {
+				$params = array(
+					'uids'         => $token['user_id'],
+					'fields'       => 'uid,first_name,last_name,bdate,',
+					'access_token' => $token['access_token']
+				);
+
+				$userInfo = json_decode(file_get_contents('https://api.vk.com/method/users.get' . '?' . urldecode(http_build_query($params))), true);
+				if (isset($userInfo['response'][0]['uid'])) {
+					$userInfo = $userInfo['response'][0];
+					$result = true;
+				}
+			}
+
+
+			if ($result) {
+				$id_vk = $userInfo['uid'];
+				$first_name = $userInfo['first_name'];
+				$last_name = $userInfo['last_name'];
+				$bdate = $userInfo['bdate'];
+				$this->make('vk');
+
+				$user = UserVk::firstOrNew(array(
+					'id_vk' => $id_vk,
+				));
+
+				$user->setAttribute('id_vk', $id_vk);
+				$user->setAttribute('first_name', $first_name);
+				$user->setAttribute('last_name', $last_name);
+				$user->save();
+
+			}
+
+		}
 	}
 }
--- app/routes.php
+++ app/routes.php
@@ -9,5 +9,4 @@ Route::get('registration', array('as' => 'registration', 'uses' => 'App\Controll
 Route::get('probation', 'App\Controllers\Site\MainController@probation');
 Route::post('probation', 'App\Controllers\Site\MainController@thank');
 
-Route::get('vk', 'App\Controllers\Site\MainController@vk');
-Route::get('test', 'App\Controllers\Site\MainController@test');
+Route::get('vk', 'App\Controllers\Site\AuthController@vk');
