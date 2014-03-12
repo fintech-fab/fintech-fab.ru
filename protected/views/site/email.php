9c88e4d8a4415dd2f9e08283c1e29485e152270a
mikhailn <m.novikov@kreddy.ru>
2014-03-12 18:10:37 +0400

    #1880 багфикс редиректа

--- protected/views/site/email.php
+++ protected/views/site/email.php
@@ -1,17 +1,18 @@
-<?
+<?php
+
 /* @var $sRequestType */
 
 $this->pageTitle = Yii::app()->name . ' - Подтверждение Email'; ?>
 
 	<h2>Подтверждение Email</h2><br>
-<?
+<?php
 if ($sRequestType === 'confirm') {
 	?>
 	<h2>Спасибо, Ваш email подтвержден</h2>
-<? } elseif ($sRequestType === 'unsubscribe') { ?>
+<?php } elseif ($sRequestType === 'unsubscribe') { ?>
 	<h2>Спасибо, Вы отписались от рассылки</h2>
-<? } elseif ($sRequestType === 'broken') { ?>
+<?php } elseif ($sRequestType === 'broken') { ?>
 	<h2>Спасибо, Ваш email не действителен</h2>
-<? } elseif ($sRequestType === 'codeError') { ?>
+<?php } elseif ($sRequestType === 'codeError') { ?>
 	<h2>Код сообщения содержит ошибку!</h2>
-<? }
+<?php }
