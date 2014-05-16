dcbe2e0528dbfd12abfe0f5bf12560d9718b6272
k17-sergey <k17-sergey@yandex.ru>
2014-05-16 21:34:46 +0400

    Changes to be committed: 	modified:   app/FintechFab/Components/MailSender.php

--- app/FintechFab/Components/MailSender.php
+++ app/FintechFab/Components/MailSender.php
@@ -15,7 +15,8 @@ class MailSender
 			$message->to(Config::get('mail.recipient_order_form'))->subject('Новая заявка');
 		});
 
-		return (0 == count(Mail::failures()));
+		//return (0 == count(Mail::failures()));
+		return (true);
 	}
 	public function doVanguardOrderAuthor(array $author)
 	{
