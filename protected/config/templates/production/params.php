10c3712f761d1eb9722b461e71c19df778c5aa40
Andrey Perepechaev <a.perepechaev@fintech-fab.ru>
2014-06-05 12:07:57 +0400

    #2450 @review

--- protected/config/templates/production/params.php
+++ protected/config/templates/production/params.php
@@ -1,14 +1,15 @@
 <?php
 
 return array(
-	'adminName'        => '%ADMIN_NAME%',
-	'adminPassword'    => '%ADMIN_PASS%',
-	'bApiTestModeIsOn' => false,
-	'bSmsGateIsOff'    => false,
-	'cryptSalt'        => '%CRYPT_SALT%',
-	'mainUrl'          => 'https://kreddy.ru',
-	'ivanovoUrl'       => 'https://ivanovo.kreddy.ru',
-	'contactEmail'     => 'operator@kreddy.ru',
-	'bShowAlert'       => false,
-	'ivPath'           => '/var/www/keys/iv',
+	'adminName'           => '%ADMIN_NAME%',
+	'adminPassword'       => '%ADMIN_PASS%',
+	'bApiTestModeIsOn'    => false,
+	'bSmsGateIsOff'       => false,
+	'cryptSalt'           => '%CRYPT_SALT%',
+	'mainUrl'             => 'https://kreddy.ru',
+	'ivanovoUrl'          => 'https://ivanovo.kreddy.ru',
+	'contactEmail'        => 'operator@kreddy.ru',
+	'bShowAlert'          => false,
+	'ivPath'              => '/var/www/keys/iv',
+	'landingApiSecretKey' => '%TORNADO_API_SECRET_KEY%',
 );
