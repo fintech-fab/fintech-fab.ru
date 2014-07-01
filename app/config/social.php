d9ba50679ca727f679f90e2e9d6c8d74cc8e2e30
Sergey <eupathy@gmail.com>
2014-07-01 15:29:14 +0400

    Исправлена настройка порта для ответа от соц сетей

--- app/config/social.php
+++ app/config/social.php
@@ -4,21 +4,21 @@ return array(
 	'vk' => array(
 		'ID'           => null,
 		'key'          => null,
-		'redirect_url' => 'http://fintech-fab.dev:8080/vk',
+		'redirect_url' => 'http://fintech-fab.dev:8000/vk',
 		'url'          => 'http://oauth.vk.com/authorize',
 	),
 	'fb' => array(
 		//Для Facebook
 		'ID'           => null,
 		'key'          => null,
-		'redirect_url' => 'http://fintech-fab.dev:8080/fb',
+		'redirect_url' => 'http://fintech-fab.dev:8000/fb',
 		'url'          => 'https://www.facebook.com/dialog/oauth',
 	),
 	'gp' => array(
 		//Для Google+
 		'ID'           => null,
 		'key'          => null,
-		'redirect_url' => 'http://localhost/fintech-fab.dev:8080/gp',
+		'redirect_url' => 'http://localhost/fintech-fab.dev:8000/gp',
 		'url'          => 'https://accounts.google.com/o/oauth2/auth',
 		'scope'        => 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile'
 	),
