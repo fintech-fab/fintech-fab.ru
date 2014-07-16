5fc3a8787ca9ab7eedaaa2536a9529a334c13bbc
Sergey <eupathy@gmail.com>
2014-07-16 23:27:20 +0400

    #12 Добавлены тесты

--- workbench/fintech-fab/actions-calc/tests/FintechFab/ActionsCalc/CalcRequestTest.php
+++ workbench/fintech-fab/actions-calc/tests/FintechFab/ActionsCalc/CalcRequestTest.php
@@ -5,7 +5,6 @@ use FintechFab\ActionsCalc\Components\SendResults;
 
 class CalcRequestTest extends CalcTestCase
 {
-	private $requestData;
 	/**
 	 * @var Mockery\MockInterface|SendResults
 	 */
@@ -16,13 +15,6 @@ class CalcRequestTest extends CalcTestCase
 		parent::setUp();
 
 		$this->mock = Mockery::mock('FintechFab\ActionsCalc\Components\SendResults');
-		$this->requestData = array(
-			'term'   => 1,
-			'sid'  => 'im_hungry',
-			'data' => json_encode(array('time' => '13.05')),
-			'signal' => null,
-		);
-
 	}
 
 	public function testGetRequest()
@@ -40,15 +32,80 @@ class CalcRequestTest extends CalcTestCase
 
 		});
 
+		$requestData = array(
+			'term'   => 1,
+			'sid'    => 'im_hungry',
+			'data'   => json_encode(array('time' => '13.05')),
+			'signal' => null,
+		);
+
 		$response = $this->call(
 		'POST',
 			'/actions-calc/getRequest',
-			$this->requestData
+			$requestData
 		);
 
-		print_r($response);
 	}
 
+	public function testGetRequest2()
+	{
+
+		App::bind('FintechFab\ActionsCalc\Components\SendResults', function () {
+			$this->mock
+				->shouldReceive('makeCurl')
+				->withArgs(['http://test', 'wait']);
+			$this->mock
+				->shouldReceive('sendQueue')
+				->withArgs(['queueTest', 'wait']);
+
+			return $this->mock;
+
+		});
+
+		$requestData = array(
+			'term'   => 1,
+			'sid'    => 'im_hungry',
+			'data'   => json_encode(array('time' => '12.05')),
+			'signal' => null,
+		);
+
+		$response = $this->call(
+			'POST',
+			'/actions-calc/getRequest',
+			$requestData
+		);
+
+	}
+
+	public function testGetRequest3()
+	{
+
+		App::bind('FintechFab\ActionsCalc\Components\SendResults', function () {
+			$this->mock
+				->shouldReceive('makeCurl')
+				->withArgs(['http://test', 'endure']);
+			$this->mock
+				->shouldReceive('sendQueue')
+				->withArgs(['queueTest', 'endure']);
+
+			return $this->mock;
+
+		});
+
+		$requestData = array(
+			'term'   => 1,
+			'sid'    => 'im_hungry',
+			'data'   => json_encode(array('time' => '14.30')),
+			'signal' => null,
+		);
+
+		$response = $this->call(
+			'POST',
+			'/actions-calc/getRequest',
+			$requestData
+		);
+
+	}
 
 	public function setDown()
 	{
