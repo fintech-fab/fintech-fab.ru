043472aa92af3691a18791de4a58f0666523e6fe
d.laptev <d.laptev@fintech-fab.ru>
2014-08-05 16:42:47 +0400

    #2871 Генерация индивидуальных условий и заявлений на займ

--- protected/modules/account/views/default/individual_conditions.php
+++ protected/modules/account/views/default/individual_conditions.php
@@ -9,26 +9,56 @@
 <?php if (!empty($aConditions['active'])) {
 	$aActive = $aConditions['active'];
 	?>
-	<div>
-		Индивидуальные условия <b>Подтвердить
-			до: <?= SiteParams::formatRusDate($aActive['dt_confirm_to'], false); ?></b>
+	<div class="alert alert-warning">
+		<h5>
+			Индивидуальные условия № <?= $aActive['contract_number'] ?>
+		</h5>
+		<?php if (isset($aActive['dt_confirm_to'])) { ?>
+			<h5>
+				<b>Подтвердить до: <?= SiteParams::formatRusDate($aActive['dt_confirm_to'], false); ?></b>
+			</h5>
+			<br />
+			<div class="center">
+				<?php
+				$this->widget('bootstrap.widgets.TbButton', array(
+					'size'        => 'large',
+					'label'       => 'Принять',
+					'htmlOptions' => array(
+						'class' => 'btn-success',
+					),
+				));
+				?>  <?php
+				$this->widget('bootstrap.widgets.TbButton', array(
+					'size'        => 'large',
+					'label'       => 'Отклонить',
+					'htmlOptions' => array(
+						'class' => 'btn-warning',
+					)
+				));
+				?>
+			</div>
+			<br />
+		<?php } else { ?>
+			<span class="alert-success"> ПОДТВЕРЖДЕНЫ </span>
+		<?php } ?>
 	</div>
-
 <?php } ?>
+<br />
 
-<br /><br />
-<div><a href="#" class="dotted" onclick="$('#archive_conditions').toggle(); return false;">Архив</a></div><br />
+<?php if (count($aConditions)) { ?>
+	<div><a href="#" class="dotted" onclick="$('#archive_conditions').toggle(); return false;">Архив</a></div><br />
 
-<table id="archive_conditions" style="display: none;">
-	<?php foreach ($aConditions['archive'] as $aCondition) { ?>
-		<tr>
-			<td><?= CHtml::link('Посмотреть', array('/account/getDocument/', 'id' => $aCondition['hash'])); ?></td>
-			<td><?= CHtml::link('Скачать', array('/account/getDocument/', 'id' => $aCondition['hash'], 'download' => '1')); ?></td>
-			<td><?= $aCondition['status'] ?></td>
-			<td><?= $aCondition['dt_add'] ?></td>
-		</tr>
-	<?php } ?>
-</table>
+	<table id="archive_conditions" style="display: none;">
+		<?php foreach ($aConditions['archive'] as $aCondition) { ?>
+			<tr>
+				<td><?= CHtml::link('Посмотреть', array('/account/getDocument/', 'id' => $aCondition['hash'])); ?></td>
+				<td><?= CHtml::link('Скачать', array('/account/getDocument/', 'id' => $aCondition['hash'], 'download' => '1')); ?></td>
+				<td>№ <?= $aCondition['contract_number'] ?></td>
+				<td><?= SiteParams::formatRusDate($aCondition['dt_add'], false); ?></td>
+			</tr>
+		<?php } ?>
+	</table>
+<?php } ?>
 
 <h4 style="margin-top: 20px;">Общие условия</h4>
 <div>
