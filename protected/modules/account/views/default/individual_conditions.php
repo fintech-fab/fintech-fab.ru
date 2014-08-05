f2d2e5fe23f3d5c8100b88d87eb120aee59f9407
d.laptev <d.laptev@fintech-fab.ru>
2014-08-05 11:25:00 +0400

    #2871 Генерация индивидуальных условий и заявлений на займ

--- protected/modules/account/views/default/individual_conditions.php
+++ protected/modules/account/views/default/individual_conditions.php
@@ -3,8 +3,23 @@
  * @var array $aConditions
  */
 ?>
-<table>
-	<?php foreach ($aConditions as $aCondition) { ?>
+
+<h4>Индивидуальные условия договора потребительского займа</h4>
+
+<?php if (!empty($aConditions['active'])) {
+	$aActive = $aConditions['active'];
+	?>
+	<div>
+		Индивидуальные условия <b>Подтвердить до: <?= date('d.m.Y', strtotime($aActive['dt_confirm_to'])); ?></b>
+	</div>
+
+<?php } ?>
+
+<br /><br />
+<div><a href="#" class="dotted" onclick="$('#archive_conditions').toggle(); return false;">Архив</a></div><br />
+
+<table id="archive_conditions" style="display: none;">
+	<?php foreach ($aConditions['archive'] as $aCondition) { ?>
 		<tr>
 			<td><?= CHtml::link('Посмотреть', array('/account/getDocument/', 'id' => $aCondition['hash'])); ?></td>
 			<td><?= CHtml::link('Скачать', array('/account/getDocument/', 'id' => $aCondition['hash'], 'download' => '1')); ?></td>
@@ -13,3 +28,8 @@
 		</tr>
 	<?php } ?>
 </table>
+
+<h4 style="margin-top: 20px;">Общие условия</h4>
+<div>
+	<?= CHtml::link('Общие условия', array('/static/docs/general_conditions.pdf')); ?>
+</div>
