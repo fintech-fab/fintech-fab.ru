c58e2c1df4627494385aabe7112098458b15f8e1
k17-sergey <k17-sergey@yandex.ru>
2014-08-08 02:39:31 +0400

    #14 API GitHub. Изменены комментарии (cherry picked from commit 8040f0d)

--- app/FintechFab/Models/GitHubModelInterface.php
+++ app/FintechFab/Models/GitHubModelInterface.php
@@ -10,12 +10,30 @@ namespace FintechFab\Models;
  */
 interface IGitHubModel
 {
-	public function getKeyName(); //Возвращает имя ключевого поля таблицы БД (может быть 'id' или иным).
-	public function getMyName();  //Нужно для отображения на экране, с какими данными выполняются операции.
+	/**
+	 * @return string  Возвращает имя ключевого поля таблицы БД (может быть 'id' или иным)
+	 */
+	public function getKeyName();
 
-	//Проверка и заполнение данных для сохранения. Возвращает true, если разрешено сохранять (иначе false)
+	/**
+	 * @return string  Нужно для отображения на экране, с какими данными выполняются операции.
+	 */
+	public function getMyName();
+
+	/**
+	 * Проверка и заполнение данных для сохранения. Возвращает true, если разрешено сохранять (иначе false)
+	 * @param $inData
+	 *
+	 * @return bool
+	 */
 	public function dataGitHub($inData);
-	//Проверка и заполнение данных для обновления. Возвращает true или false, разрешая обновлять или не разрешая.
+
+	/**
+	 * Проверка и заполнение данных для обновления. Возвращает true или false, разрешая обновлять или не разрешая.
+	 * @param $inData
+	 *
+	 * @return bool
+	 */
 	public function updateFromGitHub($inData);
 
 }
