a030646f64075757e6a3163bea00d829596ff4c4
Kirill Marenov <kmarenov@gmail.com>
2014-09-04 00:12:40 +0400

    #17 Исправление миграций после ревью: заданы значения по умолчанию, убраны внешние ключи, добавлены поля - признаки удаления записи

--- app/database/migrations/2014_09_02_234512_create_dinner_menu_users_table.php
+++ app/database/migrations/2014_09_02_234512_create_dinner_menu_users_table.php
@@ -15,19 +15,10 @@ class CreateDinnerMenuUsersTable extends Migration
 	{
 		Schema::create('dinner_menu_users', function (Blueprint $table) {
 			$table->increments('id');
-			$table->integer('user_id')->unsigned();
-			$table->integer('dinner_menu_item_id')->unsigned();
-			// в данном случае - внешние ключи - не сильно необходимы, потому что
-			// удалять пользователей и пункты меню обедов - не планируется
-			// удалять вообще плохо для индексов (проще просто отдельным полем "выключить")
-			// т.е. не плохо - что они (внешние ключи) есть. но толку никакого.
-			// если будете с базой работать вручную, или ее в юнит-тестах - будет мучение сплошное.
-			// например вы не сможете создать специально ошибочную операцию для тестирования вашего кода.
-			// лучше если целостность контролируется не базой, а вами собственноручно.
-			// т.е. у вас в коде не происходит неконтролируемых ситуаций, которые нужно защищать внешними ключами.
-			$table->foreign('user_id')->references('id')->on('users');
-			$table->foreign('dinner_menu_item_id')->references('id')->on('dinner_menu_items');
+			$table->integer('user_id')->unsigned()->default(0);
+			$table->integer('dinner_menu_item_id')->unsigned()->default(0);
 			$table->integer('count')->unsigned()->default(1);
+			$table->boolean('deleted')->default(false);
 			$table->timestamps();
 		});
 	}
