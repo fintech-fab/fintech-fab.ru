971ff992822d891b7a22b0178c585d8eab956362
Kirill Marenov <kmarenov@gmail.com>
2014-09-04 22:07:09 +0400

    #17 Команда artisan для импорта меню

---
+++ app/commands/DinnerImportMenu.php
@@ -0,0 +1,59 @@
+<?php
+
+use App\Controllers\Dinner\DinnerController;
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputArgument;
+
+class DinnerImportMenu extends Command
+{
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'dinner:import-menu';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Импорт файла меню в базу данных';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function fire()
+	{
+		$url = $this->argument('url');
+
+		if (DinnerController::importMenu($url)) $this->info('Меню успешно импортировано');
+		else $this->error('При импорте меню произошла ошибка');
+	}
+
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+	protected function getArguments()
+	{
+		return array(
+			array('url', InputArgument::REQUIRED, 'URL файла меню.'),
+		);
+	}
+
+}
