61c07fe66c95f6203c518974192c880155d89c61
mantisa1824 <mantisa1824@bk.ru>
2014-09-06 22:03:49 +0400

    #17 Работа с файлом  предаставления и работа со стилями

--- app/views/dinner/dinner.php
+++ app/views/dinner/dinner.php
@@ -2,16 +2,19 @@
 
 // Если $end_dinner не определена по умолчанию false;
 $end_dinner = empty($end_dinner) ? false : $end_dinner;
-
+// Пока у нас один юзер
+$user_id = 5;
 
 if(!$end_dinner):
 
 	// Если $end_dinner === false отрисовываем табличку для заказа обеда?>
 
+	<h3>Сделать заказ</h3>
+
 	<table class="table table-hover">
 
 		<thead>
-			<td>Название блюда</td>
+			<td class="first">Название блюда</td>
 			<td>Описание Блюда</td>
 			<td>Цена</td>
 			<td>Заказ</td>
@@ -20,14 +23,13 @@ if(!$end_dinner):
 	<?php foreach($menu as $food):?>
 
 		<tr id="this-food-id-<?=$food->id?>">
-			<td><?=$food->title?></td>
+			<td class="first"><?=$food->title?></td>
 			<td><?=$food->description?></td>
 			<td><?=$food->price?></td>
 			<td>
-				<span class="less glyphicon glyphicon-plus-sign"></span>
-				<span class="quntity"> 0 </span>
-				<span class="more glyphicon glyphicon-minus-sign"></span>
-
+				<span class="less glyphicon glyphicon-minus-sign"></span>&nbsp;
+				<span class="quntity">0</span>&nbsp;
+				<span class="more glyphicon glyphicon-plus-sign"></span>
 			</td>
 		</tr>
 
@@ -35,13 +37,21 @@ if(!$end_dinner):
 
 	</table>
 
+	<input type="hidden" id="user-id" value="<?=$user_id?>" />
+
 <?php else:
 
 	// Если $end_dinner === true значит заказ обедов окончен ?>
 
 	<div id="end-dinner">
-		<p>Извините , в данное время сделать заказ невозможно!.</p>
-		<p onclick=" window.history.back() ">Назад</p>
+		<h3>Недоступно</h3>
+		<div>Заказ обеда можно сделать в период с 8.00 до 16.00 .<br/>
+			Текущие время : <span id="show-time"></span>
+		</div>
+		<br/>
+		<div>
+			<button type="button" class="btn btn-danger" onclick="window.history.back()">Назад</button>
+		</div>
 	</div>
 
 <?php endif;?>
--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local'      => array('fintech-fab', '*local', 'homestead'),
+	'local'      => array('fintech-fab', '*local', 'homestead' , 'home'),
 	'production' => array('LXC01'),
 
 ));
