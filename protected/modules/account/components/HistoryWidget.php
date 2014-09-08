94ccbfef8ca9a7430cca7caaf4fef9e9a97459b4
d.laptev <d.laptev@fintech-fab.ru>
2014-09-08 15:44:54 +0400

    #3077 История операций

--- protected/modules/account/components/HistoryWidget.php
+++ protected/modules/account/components/HistoryWidget.php
@@ -25,16 +25,18 @@ class HistoryWidget extends TbGridView
 	const PRODUCT_TYPE_IVANOVO = 2;
 	const PRODUCT_TYPE_KREDDYLINE = 3;
 	const PRODUCT_TYPE_KREDDYLINE_POSTPAID = 4;
+	const PRODUCT_TYPE_KREDDYLINE_PERCENT_PREPAID = 5;
+	const PRODUCT_TYPE_KREDDYLINE_PERCENT_POSTPAID = 6;
 
 	protected static $aOperationTypes = array(
-		self::PRODUCT_TYPE_KREDDY              => array(
+		self::PRODUCT_TYPE_KREDDY                      => array(
 			self::C_INVOICE_TYPE_SUBSCRIPTION      => 'Оплата Пакета',
 			self::C_INVOICE_TYPE_LOAN              => 'Возврат суммы перевода',
 			self::C_INVOICE_TYPE_LOAN_FINE         => 'Частичное погашение задолженности (пени, сумма перевода)',
 			self::C_INVOICE_TYPE_LOAN_PENALTY      => 'Частичное погашение задолженности (штраф, сумма перевода)',
 			self::C_INVOICE_TYPE_LOAN_FINE_PENALTY => 'Частичное погашение задолженности (штраф, пени, сумма перевода)',
 		),
-		self::PRODUCT_TYPE_IVANOVO             => array(
+		self::PRODUCT_TYPE_IVANOVO                     => array(
 			self::C_INVOICE_TYPE_SUBSCRIPTION      => 'Оплата займа',
 			self::C_INVOICE_TYPE_LOAN              => 'Погашение займа',
 			self::C_INVOICE_TYPE_LOAN_FINE         => 'Частичное погашение задолженности (пени, сумма перевода)',
@@ -42,19 +44,33 @@ class HistoryWidget extends TbGridView
 			self::C_INVOICE_TYPE_LOAN_FINE_PENALTY => 'Частичное погашение задолженности (штраф, пени, сумма перевода)',
 			self::C_INVOICE_TYPE_LOAN_FINE_PENALTY => 'Частичное погашение задолженности (штраф, пени, сумма перевода)',
 		),
-		self::PRODUCT_TYPE_KREDDYLINE          => array(
+		self::PRODUCT_TYPE_KREDDYLINE                  => array(
 			self::C_INVOICE_TYPE_SUBSCRIPTION      => 'Абонентская плата',
 			self::C_INVOICE_TYPE_LOAN              => 'Возврат суммы перевода',
 			self::C_INVOICE_TYPE_LOAN_FINE         => 'Частичное погашение задолженности (пени, сумма перевода)',
 			self::C_INVOICE_TYPE_LOAN_PENALTY      => 'Частичное погашение задолженности (штраф, сумма перевода)',
 			self::C_INVOICE_TYPE_LOAN_FINE_PENALTY => 'Частичное погашение задолженности (штраф, пени, сумма перевода)',
 		),
-		self::PRODUCT_TYPE_KREDDYLINE_POSTPAID => array(
+		self::PRODUCT_TYPE_KREDDYLINE_POSTPAID         => array(
 			self::C_INVOICE_TYPE_SUBSCRIPTION_POSTPAID          => 'Абонентская плата',
 			self::C_INVOICE_TYPE_LOAN_SUBSCRIPTION              => 'Абонентская плата и сумма перевода',
 			self::C_INVOICE_TYPE_LOAN_SUBSCRIPTION_FINE         => 'Абонентская плата, сумма перевода и пени',
 			self::C_INVOICE_TYPE_LOAN_SUBSCRIPTION_PENALTY      => 'Абонентская плата, сумма перевода и штраф',
 			self::C_INVOICE_TYPE_LOAN_SUBSCRIPTION_FINE_PENALTY => 'Абонентская плата, сумма перевода, штраф и пени',
+		),
+		self::PRODUCT_TYPE_KREDDYLINE_PERCENT_PREPAID  => array(
+			self::C_INVOICE_TYPE_SUBSCRIPTION      => 'Внесение абонентской платы',
+			self::C_INVOICE_TYPE_LOAN              => 'Погашение задолженности',
+			self::C_INVOICE_TYPE_LOAN_FINE         => 'Погашение задолженности',
+			self::C_INVOICE_TYPE_LOAN_PENALTY      => 'Погашение задолженности',
+			self::C_INVOICE_TYPE_LOAN_FINE_PENALTY => 'Погашение задолженности',
+		),
+		self::PRODUCT_TYPE_KREDDYLINE_PERCENT_POSTPAID => array(
+			self::C_INVOICE_TYPE_SUBSCRIPTION_POSTPAID          => 'Внесение абонентской платы',
+			self::C_INVOICE_TYPE_LOAN_SUBSCRIPTION              => 'Погашение задолженности',
+			self::C_INVOICE_TYPE_LOAN_SUBSCRIPTION_FINE         => 'Погашение задолженности',
+			self::C_INVOICE_TYPE_LOAN_SUBSCRIPTION_PENALTY      => 'Погашение задолженности',
+			self::C_INVOICE_TYPE_LOAN_SUBSCRIPTION_FINE_PENALTY => 'Погашение задолженности',
 		),
 	);
 
