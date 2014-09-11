c2d4c296850b4980c7aeb4a1a1dcc26f080973f5
Kirill Marenov <kmarenov@gmail.com>
2014-09-11 22:36:01 +0400

    #17 Алиас для Maatwebsite\Excel\Facades\Excel

--- app/config/app.php
+++ app/config/app.php
@@ -180,7 +180,7 @@ return array(
 		'URL'         => 'Illuminate\Support\Facades\URL',
 		'Validator'   => 'Illuminate\Support\Facades\Validator',
 		'View'        => 'Illuminate\Support\Facades\View',
-
+		'Excel' => 'Maatwebsite\Excel\Facades\Excel',
 	),
 
 );
