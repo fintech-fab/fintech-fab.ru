9d9433f352bf675470d17a305e8c66f2582d4b4b
Andrey Perepechaev <a.perepechaev@fintech-fab.ru>
2014-09-12 18:25:32 +0400

    #3012

--- protected/extensions/pixels/views/linkprofit.php
+++ protected/extensions/pixels/views/linkprofit.php
@@ -5,6 +5,6 @@
 
 ?>
 <!-- linkprofit tracking start -->
-<img src="https://linkprofit.ru/scripts/sale.php?AccountId=default1&OrderID=<?= CHtml::encode($aParams['iOrderId']) ?>&ProductID=1701e8fb&CampaignID=1701e8fb&AffiliateID=<?= CHtml::encode($aParams['wid']) ?>" width="1" height="1">
+<img src="https://www.linkprofit.ru/scripts/sale.php?AccountId=default1&TotalCost=50&OrderID=<?= CHtml::encode($aParams['iOrderId']) ?>&ProductID=1701e8fb&CampaignID=1701e8fb&AffiliateID=<?= CHtml::encode($aParams['wid']) ?>" width="1" height="1">
 
 <!-- linkprofit tracking end -->
